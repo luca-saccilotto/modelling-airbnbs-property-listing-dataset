4de8ae10c5e575d82e2bd17a99d7a93332ec5a12
“luca-saccilotto” <luca.sacc@outlook.com>
2023-03-04 18:52:56 +0100

    Adds Task 2

--- modelling.py
+++ modelling.py
@@ -2,24 +2,35 @@
 import numpy as np
 import pandas as pd
 from tabular_data import load_airbnb
-from sklearn import model_selection, linear_model
+from sklearn import linear_model, model_selection, metrics
 
-# Define the path to the file and print its data type
-df = pd.read_csv("airbnb-property-listings/tabular_data/clean_tabular_data.csv")
-print(type(df))
+# Define the path to the file that contains the data
+df = pd.read_csv("airbnb-property-listings/tabular_data/clean_tabular_data.csv", index_col = "ID")
 
-# Load the data into two different variables
+# Load and split the data into training, validation, and test sets
 X, y = load_airbnb(df, "Price_Night")
-# X = X.select_dtypes(include = "number")
+np.random.seed(1)
 
-# Split the data into training, validation, and test sets
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.3)
 X_validation, X_test, y_validation, y_test = model_selection.train_test_split(X_test, y_test, test_size = 0.5)
 
-# Create a simple regression model to predict the nightly cost of each listing
-sgdr = linear_model.SGDRegressor()
-sgdr.fit(X_train, y_train)
+print(X_train.shape, y_train.shape)
 
-# Compute the R-Squared score of the model on the training data
-score = sgdr.score(X_train, y_train)
-print("R-Squared:", score)
+# Create a simple regression model to predict the nightly cost of each listing
+regressor = linear_model.SGDRegressor()
+regressor.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = regressor.predict(X_test)
+
+# Calculate the mean squared error of the predictions
+mse = metrics.mean_squared_error(y_test, y_pred)
+print("MSE: ", mse)
+
+# Calculate the root mean squared error
+rmse = np.sqrt(mse)
+print("RMSE: ", rmse)
+
+# Compute the R-Squared of the model on the training data
+r2 = regressor.score(X_train, y_train)
+print("R-Squared: ", r2)
--- tabular_data.py
+++ tabular_data.py
@@ -10,7 +10,7 @@ def remove_rows_with_missing_ratings(df) -> pd.DataFrame:
                              "Communication_rating",
                              "Location_rating",
                              "Check-in_rating",
-                             "Value_rating"], how = "any")
+                             "Value_rating"])
     return df
 
 # Define a function that combines the list items into the same string
@@ -18,7 +18,7 @@ def combine_description_strings(df) -> pd.DataFrame:
     
     def convert_description_strings(x: str):
         """
-        Converts a list of strings to a single string by
+        Convert a list of strings to a single string by
         concatenating the remaining strings
         
         """
@@ -28,7 +28,7 @@ def combine_description_strings(df) -> pd.DataFrame:
             x = "".join(x)
             return x
         except:
-            print(f"Failed to convert string: {x}")
+            # print(f"Failed to convert string: {x}")
             return np.nan
     
     df["Description"] = df["Description"].dropna().apply(convert_description_strings)
@@ -38,7 +38,7 @@ def combine_description_strings(df) -> pd.DataFrame:
 # Define a function to fill missing values with 1 in specific columns
 def set_default_feature_values(df) -> pd.DataFrame:
     cols_to_fill = ["guests", "beds", "bathrooms", "bedrooms"]
-    df[cols_to_fill] = df[cols_to_fill].fillna(1).astype("int64")
+    df[cols_to_fill] = df[cols_to_fill].apply(pd.to_numeric, errors = "coerce").fillna(1)
     return df
 
 # Define a function that returns the features and labels of your data in a tuple
@@ -49,21 +49,25 @@ def load_airbnb(df, labels) -> pd.DataFrame:
 
 # Encapsulate all the processing code inside a function
 def clean_tabular_data(df) -> pd.DataFrame:
-    # df.drop("ID", axis = 1, inplace = True)
-    df.drop("Unnamed: 19", axis = 1, inplace = True)
-    df.drop("4c917b3c-d693-4ee4-a321-f5babc728dc9", inplace = True)
+    """
+    Clean tabular data by removing rows with missing ratings,
+    combining description strings, and setting default feature values
+
+    """
     remove_rows_with_missing_ratings(df)
     combine_description_strings(df)
     set_default_feature_values(df)
-    df = df.dropna()
-    df = df.select_dtypes(include = "number")
+
+    """Return a dataframe contains only columns with numerical data types"""
+    df.drop("Unnamed: 19", axis = 1, inplace = True)
+    df = df.select_dtypes(include = ["int64", "float64"])
+
     return df
 
 # Ensure that the code inside it is only executed if the script is being run as the main program
 if __name__ == "__main__":
-    path = "airbnb-property-listings/tabular_data/listing.csv"
     try:
-        raw_data = pd.read_csv(path, index_col = "ID")
+        raw_data = pd.read_csv("airbnb-property-listings/tabular_data/listing.csv", index_col = "ID")
     except:
         print("Failed to read input file")
     df = clean_tabular_data(raw_data)
